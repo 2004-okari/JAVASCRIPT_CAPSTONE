c5c3ada6b31794a4607056a2853a0210139bfa58
okari <nyandika15@gmail.com>
2023-07-31 23:30:18 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+
+import displayMenu from './modules/displayMenu.js';
+
+const apiUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+
+document.addEventListener('DOMContentLoaded', displayMenu);
